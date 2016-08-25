9c91c841161ab2b4abeca602014135b7b886bc29
Nicholas Thompson <nick@fullfatthings.com>
2016-08-26 00:57:35 +0100

    Update 2016-08-25T23:57:30.042Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,19 @@
-"use strict";
+'use strict';
+
+$(function () {
+    setNavigation();
+});
+
+function setNavigation() {
+    var path = window.location.pathname;
+    path = path.replace(/\/$/, '');
+    path = decodeURIComponent(path);
+
+    $('.nav a').each(function () {
+        var href = $(this).attr('href');
+        if (path.substring(1, href.length + 1) === href) {
+            $(this).closest('li').addClass('active');
+        }
+    });
+}
 //# sourceMappingURL=main.js.map
---
+++ templates/foo.js
@@ -0,0 +1,13 @@
+this["IHD"] = this["IHD"] || {};
+this["IHD"]["templates"] = this["IHD"]["templates"] || {};
+this["IHD"]["templates"]["portfolio"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<div class=\"portfolio-item\">\n  <div class=\"inner\">\n    <img src=\""
+    + alias4(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"image","hash":{},"data":data}) : helper)))
+    + "\" />\n    <div class=\"info\">\n      <h2>"
+    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</h2>\n      <h3>"
+    + alias4(((helper = (helper = helpers.type || (depth0 != null ? depth0.type : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"type","hash":{},"data":data}) : helper)))
+    + "</h3>\n    </div>\n  </div>\n</div>\n";
+},"useData":true});;
