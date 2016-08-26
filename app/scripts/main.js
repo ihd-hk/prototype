a04bdfeff8319768167c1c2c17d942ca90211a80
Nicholas Thompson <nick@fullfatthings.com>
2016-08-26 11:15:22 +0100

    Adding Brick JS and some filtering. Adding locations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,10 +4,10 @@ $(function () {
 
 function setNavigation() {
     var path = window.location.pathname;
-    path = path.replace(/\/$/, "");
+    path = path.replace(/\/$/, '');
     path = decodeURIComponent(path);
 
-    $(".nav a").each(function () {
+    $('.header .nav a').each(function () {
         var href = $(this).attr('href');
         if (path.substring(1, href.length+1) === href) {
             $(this).closest('li').addClass('active');
