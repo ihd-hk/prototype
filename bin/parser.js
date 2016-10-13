862176de7aa3f552a3d6da030c1ee7ef9439c719
Nicholas Thompson <nick@fullfatthings.com>
2016-10-13 07:13:46 +0100

    Updating scripts... New column in spreadsheet + resize thumbnails to square.

--- bin/parser.js
+++ bin/parser.js
@@ -14,7 +14,7 @@ var worksheet = workbook.Sheets.WEBSITE;
 
 
 data = {
-  items: XLSX.utils.sheet_to_json(worksheet, { header: ['project', 'date', 'title_en', 'title_ch', 'disciplines', 'codes', 'city_en', 'city_ch', 'country_en', 'country_ch', 'hotel_en', 'hotel_ch', 'desc_en', 'desc_ch'], range: 6 }),
+  items: XLSX.utils.sheet_to_json(worksheet, { header: ['id', 'project', 'date', 'title_en', 'title_ch', 'disciplines', 'codes', 'city_en', 'city_ch', 'country_en', 'country_ch', 'hotel_en', 'hotel_ch', 'desc_en', 'desc_ch'], range: 6 }),
   disciplines: {
     A: 'Audio-Visual',
     C: 'Acoustics',
@@ -59,7 +59,6 @@ data.items.forEach(function(item, index, items) {
   }
   items[index].files = glob.sync(item.project + '-*.jpg', {cwd: 'app/images/portfolio', nocase: true});
 });
-// data.countries = _.sortBy(data.countries);
 
 
 fs.writeFileSync('app/scripts/portfolio-data.json', JSON.stringify(data), 'utf-8');
--- bin/portfolio-thumbs.sh
+++ bin/portfolio-thumbs.sh
@@ -3,4 +3,4 @@
 IN_PATH="$1"
 OUT_PATH="$2"
 
-mogrify  -format jpg  -verbose  -auto-orient  -thumbnail "400x200^"  -gravity center  -crop 400x200+0+0  +repage  -path "$OUT_PATH"  -unsharp 0x.5 "$IN_PATH/*.jpg"
+mogrify  -format jpg  -verbose  -auto-orient  -thumbnail "400x400^"  -gravity center  -crop 400x400+0+0  +repage  -path "$OUT_PATH"  -unsharp 0x.5 "$IN_PATH/*.jpg"
